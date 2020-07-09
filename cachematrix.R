efa2039daa21ee3b112255d662a97f39944ef665
Ricardorg98 <65617071+Ricardorg98@users.noreply.github.com>
2020-07-09 09:09:49 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ ##let's make the makeCacheMatrix and cacheSolve function true
+> makeCacheMatrix <- function(x = matrix()){
++         inv <- NULL
++         set <- function(y){
++                 x <<- y
++                 inv <<- NULL
++         }
++         get <- function() {x}
++         setInverse <- function(inverse) {inv <<- inverse}
++         getInverse <- function() {inv}
++         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
++ }
+> cacheSolve <- function(x, ...){
++         inv <- x$getInverse()
++         if(!is.null(inv)){
++                 message("getting cached data")
++                 return(inv)
++         }
++         mat <- x$get()
++         inv <- solve(mat, ...)
++         x$setInverse(inv)
++         inv
++ }
+> ## forming a 4x4 matrix that goes from 1 to 16, using the following code:
+> source("makeCacheMatrix.R")  
+> matrix<- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+> ##See the matrix:
+> matrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> ##And now Inverse Matrix:
+> matrix$getInverse()
+NULL
+> cacheSolve(matrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
